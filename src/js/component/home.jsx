93d581095f51e367faae93cd22488335677384e7
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-03-07 16:20:13 +0000

    Proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 
+
 const Home = () => {
-  const [color, setColor] = useState("red");
+  const [color, setColor]= useState("")
+  const colors = ["red", "yellow", "green"];
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
 
-  const handleClick = (selectedColor) => {
-    setColor(selectedColor);
+  const changeLight = (newColor) => {
+   setColor (newColor);
   };
 
-  const renderLight = (lightColor) => {
-    return (
-      <div
-        className={`light ${color === lightColor ? "on" : ""}`}
-        onClick={() => handleClick(lightColor)}
-      />
-    );
+  const toggleColor = () => {
+    setCurrentColorIndex((currentColorIndex + 1) % colors.length);
+    setColor(colors[currentColorIndex]);
   };
 
-  return (
-    <div className="traffic-light">
-      <div className="pole">
-        {renderLight("red")}
-        {renderLight("yellow")}
-        {renderLight("green")}
+  return (   
+    <div className="traffic-light-container-fluid text-center">
+      <div className="traffic-light">
+        <button className={`light red ${color === 'red' ? 'active' : ''}`} onClick={() => changeLight('red')}></button>
+        <button className={`light yellow ${color === 'yellow' ? 'active' : ''}`} onClick={() => changeLight('yellow')}></button>
+        <button className={`light green ${color === 'green' ? 'active' : ''}`} onClick={() => changeLight('green')}></button>
       </div>
-      <button onClick={() => handleClick("red")}>Rojo</button>
-      <button onClick={() => handleClick("yellow")}>Amarillo</button>
-      <button onClick={() => handleClick("green")}>Verde</button>
-      <button onClick={() => setColor("purple")}>Púrpura</button>
+      <br />
+      <button className="blue-button" onClick={toggleColor}>Cambiar Color</button>
     </div>
   );
 };
